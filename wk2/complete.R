9440b7aa5f483ab52bc6bf6619d7043241757bae
frankfurt <frankfurtmacmoses@gmail.com>
2015-02-04 09:34:14 +0100

    First Assignment commit

---
+++ wk2/complete.R
@@ -0,0 +1,14 @@
+complete <- function(directory,id = 1:332 ){
+   files <- list.files(directory, full.names = TRUE) 
+   d_frame <- data.frame()
+   for (i in id){
+      file_content <- read.csv(files[i]) 
+      obs <- nrow(file_content)
+      com_col <- c(i,obs)
+      d_frame <- rbind(d_frame,com_col)
+           
+           
+   }
+   colnames(d_frame) <- c('id','nobs')     
+    d_frame    
+}
